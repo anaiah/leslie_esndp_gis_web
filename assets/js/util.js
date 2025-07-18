8f41040442ea8e136d59a9196992ceaf356b8e06
anaiah <carlodominguez@yahoo.com>
2025-07-18 10:04:52 +0800

    x2

--- assets/js/util.js
+++ assets/js/util.js
@@ -7,8 +7,8 @@ modals,forms,utilities
 
 */
 
-const myIp = "http://10.202.213.221:10000"
-//const myIp  = "https://esndp-gis-jku4q.ondigitalocean.app"
+//const myIp = "http://10.202.213.221:10000"
+const myIp  = "https://esndp-gis-jku4q.ondigitalocean.app"
 
 const requirements = document.querySelectorAll(".requirements")
 const specialChars = "!@#$%^&*()-_=+[{]}\\| :'\",<.>/?`~"
