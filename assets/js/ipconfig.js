978dc003d637569eca5086951003e27815fdf8c5
anaiah <carlodominguez@yahoo.com>
2025-07-21 11:04:49 +0800

    xx

--- assets/js/ipconfig.js
+++ assets/js/ipconfig.js
@@ -1,3 +1,3 @@
 
-const myIp = "http://10.202.213.221:10000"
-//const myIp  = "https://esndp-gis-jku4q.ondigitalocean.app"
+//const myIp = "http://10.202.213.221:10000"
+const myIp  = "https://esndp-gis-jku4q.ondigitalocean.app"
