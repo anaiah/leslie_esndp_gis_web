241a4d3c8d35eebfdce40c08a48c3d7842ad5ced
anaiah <carlodominguez@yahoo.com>
2025-07-24 20:21:53 +0800

    csss

--- assets/js/base.js
+++ assets/js/base.js
@@ -457,19 +457,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
     ///disable  rightclck
     document.onkeydown = function(e) {
-    if(e.keyCode == 123) {  // F12
-        return false;
+        if(e.keyCode == 123) {  // F12
+            return false;
+        }
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 73) {  // Ctrl+Shift+I
+            return false;
+        }
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {  // Ctrl+Shift+J
+            return false;
+        }
+        if(e.ctrlKey && e.keyCode == 85) { // Ctrl+U
+            return false;
+        }
     }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 73) {  // Ctrl+Shift+I
-        return false;
-    }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {  // Ctrl+Shift+J
-        return false;
-    }
-    if(e.ctrlKey && e.keyCode == 85) { // Ctrl+U
-        return false;
-    }
-}
+
    
 })
    
